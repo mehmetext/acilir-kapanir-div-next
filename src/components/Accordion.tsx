5c1e9c0d90419e3772f3f0095e9a3fde33afad9f
Mehmet Konukcu <mehmetext68@gmail.com>
2023-04-03 01:07:25 +0300

    content ref, isOpen ve height eklendi. Animasyonlu bir şekilde kapanıp açılıyor.

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,10 +1,34 @@
+import { useEffect, useRef, useState } from "react";
+
 export default function Accordion() {
+	const content = useRef<HTMLDivElement>(null);
+
+	const [isOpen, setIsOpen] = useState(false);
+	const [height, setHeight] = useState("0px");
+
+	function toggleIsOpen() {
+		setIsOpen(!isOpen);
+	}
+
+	useEffect(() => {
+		if (content?.current) {
+			setHeight(isOpen ? `${content.current.scrollHeight}px` : "0px");
+		}
+	}, [isOpen]);
+
 	return (
 		<div className="flex flex-col border border-blue-500 rounded-xl overflow-hidden">
-			<div className="bg-blue-500 text-white p-4 select-none cursor-pointer">
+			<div
+				className="bg-blue-500 text-white p-4 select-none cursor-pointer"
+				onClick={toggleIsOpen}
+			>
 				AccordionHeader
 			</div>
-			<div className="overflow-hidden transition-all duration-300">
+			<div
+				ref={content}
+				style={{ maxHeight: height }}
+				className="overflow-hidden transition-all duration-300"
+			>
 				<div className="p-4 bg-blue-50">
 					<p>
 						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti
